468aaf6c7e3e3fc39efdadb04cf85603b04761de
Alexandra Faller <alex@faller.us>
2020-07-27 21:25:36 +0100

    Updates

---
+++ precache-manifest.49bcf7ce8939fd33aafbe2dc49a58b7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1662c30155b0a94a619078af6c890c",
+    "url": "/quote-generator-react/index.html"
+  },
+  {
+    "revision": "bae8c2bce28c04c7bced",
+    "url": "/quote-generator-react/static/js/2.84c7b23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-generator-react/static/js/2.84c7b23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9d6d0b056259b6f3bc",
+    "url": "/quote-generator-react/static/js/main.9e0ad949.chunk.js"
+  },
+  {
+    "revision": "7406f9467e2e698fc253",
+    "url": "/quote-generator-react/static/js/runtime-main.880ec01f.js"
+  }
+]);
