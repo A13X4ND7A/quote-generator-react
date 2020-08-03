561d7bca0a6f18aded8b696ec3b80d1baee10e37
Alexandra Faller <alex@faller.us>
2020-08-03 19:45:15 +0100

    added functionality to get the api to show a new quote and the quote author on page load or refresh.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Quote from './Quote';
 
+
 function App() {
     return (
         <div>
             <Quote />
+            
         </div>
     );
 }
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,11 +1,52 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import "./Quote.css"
+import axios from 'axios';
 
-function Quote(){
+const Quote = () => {
+const [authorResult, setAuthorResult] = useState('');
+const [quoteResult, setQuoteResult] = useState('');
+
+    useEffect(() => {
+        const search = async () => {
+          const {data} = await axios.get('https://shrouded-sea-82269.herokuapp.com/https://api.forismatic.com/api/1.0/', {
+                params: {
+                    method: 'getQuote',
+                    format: 'json',
+                    lang: 'en'
+                }
+            });
+            setQuoteResult(data.quoteText);
+            setAuthorResult(data.quoteAuthor);
+        };
+        search();
+    });
     return (
-        <div>
-            <h1>Hello!</h1>
+        <div className="quote-container" id="quote-container">
+
+            <div className="quote-text">
+                <i className="fas fa-quote-left"></i>
+                <span id="quote">{quoteResult}</span>
+            </div>
+
+            <div className="quote-author">
+                <span id="author">{authorResult}</span>
+            </div>
+
+            { <div className="button-container">
+                <button className="twitter-button" id="twitter" title="Tweet this!!">
+                    <i className="fab fa-twitter"></i>
+                </button>
+                <button 
+                id="new-quote"
+                onClick={getNewQuote}
+                >
+                New Quote
+                </button>
+            </div> }
         </div>
-    )
-}
+    );
+
+};
 
 export default Quote;
+
